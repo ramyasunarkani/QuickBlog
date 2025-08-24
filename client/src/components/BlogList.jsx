defe26ec73351810abaf50bdc3dc9e73b33cd3d7
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-08-24 11:41:45 +0530

    edits

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -14,6 +14,7 @@ const BlogList = () => {
       return blog.title.toLowerCase().includes(input.toLowerCase()) || blog.category.toLowerCase.includes(input.toLowerCase());
     })
   }
+
   return (
     <div>
       <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>        
@@ -38,6 +39,7 @@ const BlogList = () => {
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
             {/* ---- blog cards ----   */}
+           
             {
                 filteredBlogs().filter((blog)=>menu=="All"?true:blog.category==menu).map((blog)=>(
                        <BlogCard key={blog._id} blog={blog}>                          
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv/config";
+import connectDB from "./configs/db.js"
+import cors from "cors";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+const app=express();
+
+// dotenv.config();
+
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+      res.send("API is Working");
+});
+
+app.use('/api/admin',adminRouter);
+app.use('/api/blog',blogRouter);
+const PORT=process.env.PORT || 3000;
+
+// app.use("*", (req, res) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found",
+//   });
+// });
+
+app.listen(PORT,()=>{
+    console.log(`Server started running at http://localhost:${PORT}`)
+})
+
+export default app;
